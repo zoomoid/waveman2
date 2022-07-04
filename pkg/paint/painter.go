eb4edaec3dde260244b5d7a45f80bc83b5e4a7f9
Alexander Bartolomey <occloxium@gmail.com>
2022-07-05 00:06:21 +0200

    feat: add paint module for specifying painters
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

---
+++ pkg/paint/painter.go
@@ -0,0 +1,15 @@
+package paint
+
+type PainterOptions struct {
+	// Data contains all sample points to use in a drawing context
+	Data []float64
+}
+
+type Painter interface {
+	// TotalHeight is the interface function for getting the painter canvas's total height
+	TotalHeight() float64
+	// TotalHeight is the interface function for getting the painter canvas's total width
+	TotalWidth() float64
+	// Draw is the interface function for converting a slice of samples into SVG elements
+	Draw() []string
+}
