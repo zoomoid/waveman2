bb7896a279ec8a85968c02cb31b782a145e007a2
Alexander Bartolomey <occloxium@gmail.com>
2022-07-05 20:23:59 +0200

    fix: svg template binding types and indentation
    though element indentation needs some work still
    
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- pkg/svg/svg.go
+++ pkg/svg/svg.go
@@ -9,33 +9,33 @@ import (
 
 // DefaultSvgTemplate contains the Golang template to be executed with elements
 var DefaultSvgTemplate = dedent.Dedent(`
-	<svg 
-		baseProfile="tiny"
-		preserveAspectRatio="{{ .PreserveAspectRatio }}"
-		version="1.2"
-		viewBox="0 0 {{ .Width }} {{ .Height }}"
-		height="100%" width="100%"
-		xmlns="http://www.w3.org/2000/svg"
-		xmlns:ev="http://www.w3.org/2001/xml-events"
-		xmlns:xlink="http://www.w3.org/1999/xlink"
-	>
-	{{- range $el, _ := .Elements -}}
-		{{ $el }}
-	{{- end -}}
-	</svg>
+  <svg 
+    baseProfile="tiny"
+    preserveAspectRatio="{{ .PreserveAspectRatio }}"
+    version="1.2"
+    viewBox="0 0 {{ .Width }} {{ .Height }}"
+    height="100%" width="100%"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:ev="http://www.w3.org/2001/xml-events"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+  >
+    {{ range $_, $el := .Elements -}}
+    {{ $el }}
+    {{ end }}
+  </svg>
 `)
 
 type TemplateBindings struct {
 	PreserveAspectRatio bool
-	Width               float32
-	Height              float32
+	Width               float64
+	Height              float64
 	Elements            []string
 }
 
 // Template executes the default SVG template and writes all previously created SVG elements to the body
 // Returns the template as string
 // Returns an error if any failures occur.
-func Template(elements []string, elWidth float32, elHeight float32, preserveAspectRatio bool) (string, error) {
+func Template(elements []string, elWidth float64, elHeight float64, preserveAspectRatio bool) (string, error) {
 	tmpl, err := template.New("svg").Parse(DefaultSvgTemplate)
 	if err != nil {
 		return "", err
@@ -43,7 +43,7 @@ func Template(elements []string, elWidth float32, elHeight float32, preserveAspe
 
 	bindings := &TemplateBindings{
 		PreserveAspectRatio: preserveAspectRatio,
-		Width:               elWidth * float32(len(elements)),
+		Width:               elWidth,
 		Height:              elHeight,
 		Elements:            elements,
 	}
