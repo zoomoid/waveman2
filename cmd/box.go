ed77b38127afb460684f40a424f7f3805b8ea24a
Alexander Bartolomey <occloxium@gmail.com>
2022-07-11 15:58:48 +0200

    refactor: move reference painter implementations to their own files
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- cmd/box.go
+++ cmd/box.go
@@ -1,6 +1,89 @@
 package cmd
 
-import "github.com/zoomoid/waveman/v2/pkg/paint/box"
+import (
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/pflag"
+	"github.com/zoomoid/waveman/v2/cmd/options"
+	cmdutils "github.com/zoomoid/waveman/v2/cmd/utils"
+	"github.com/zoomoid/waveman/v2/cmd/validation"
+	"github.com/zoomoid/waveman/v2/pkg/painter/box"
+)
+
+var _ Plugin = &BoxPainter{}
+
+var BoxPainterPlugin Plugin = &BoxPainter{
+	data:    newBoxData(),
+	enabled: false,
+}
+
+const (
+	BoxMode        string = "box"
+	BoxDescription string = "Create a box waveform"
+)
+
+type BoxPainter struct {
+	data    *boxData
+	enabled bool
+}
+
+func (b *BoxPainter) Name() string {
+	return BoxMode
+}
+
+func (b *BoxPainter) Description() string {
+	return BoxDescription
+}
+
+func (b *BoxPainter) Enabled() *bool {
+	return &b.enabled
+}
+
+func (b *BoxPainter) Data() interface{} {
+	return b.data
+}
+
+func (b *BoxPainter) Validate() error {
+	errs := b.data.validateBoxOptions()
+	errlist := cmdutils.NewErrorList(errs)
+	if errlist == nil {
+		return nil
+	}
+	return errors.New(errlist.Error())
+}
+
+func (b *BoxPainter) Flags(flags *pflag.FlagSet) {
+	data, ok := b.Data().(boxData)
+	if !ok {
+		log.Fatal().Msg("box data struct is malformed")
+	}
+	flags.BoolVar(b.Enabled(), BoxMode, false, BoxDescription)
+	flags.StringVar(&data.color, options.BoxFill, box.DefaultColor, options.BoxFillDescription)
+	flags.StringVar(&data.alignment, options.Alignment, string(box.DefaultAlignment), options.AlignmentDescription)
+	flags.Float64VarP(&data.height, options.BoxHeight, options.BoxHeightShort, box.DefaultHeight, options.BoxHeightDescription)
+	flags.Float64VarP(&data.width, options.BoxWidth, options.BoxWidthShort, box.DefaultWidth, options.BoxWidthDescription)
+	flags.Float64VarP(&data.rounded, options.BoxRounded, options.BoxRoundedShort, box.DefaultRounded, options.BoxRoundedDescription)
+	flags.Float64Var(&data.gap, options.BoxGap, box.DefaultGap, options.BoxGapDescription)
+}
+
+func (b *boxData) validateBoxOptions() (errList []error) {
+	if err := validation.ValidateAlignment(b.alignment); err != nil {
+		errList = append(errList, err)
+	}
+	if err := validation.ValidateBoxHeight(b.height); err != nil {
+		errList = append(errList, err)
+	}
+	if err := validation.ValidateBoxWidth(b.width); err != nil {
+		errList = append(errList, err)
+	}
+	if err := validation.ValidateGap(b.gap, b.width); err != nil {
+		errList = append(errList, err)
+	}
+	if len(errList) == 0 {
+		return nil
+	}
+	return errList
+}
 
 type boxData struct {
 	color     string
@@ -21,7 +104,3 @@ func newBoxData() *boxData {
 		gap:       box.DefaultRounded,
 	}
 }
-
-const (
-	BoxMode string = "box"
-)
--- cmd/line.go
+++ cmd/line.go
@@ -1,6 +1,88 @@
 package cmd
 
-import "github.com/zoomoid/waveman/v2/pkg/paint/line"
+import (
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/pflag"
+	"github.com/zoomoid/waveman/v2/cmd/options"
+	cmdutils "github.com/zoomoid/waveman/v2/cmd/utils"
+	"github.com/zoomoid/waveman/v2/cmd/validation"
+	"github.com/zoomoid/waveman/v2/pkg/painter/line"
+)
+
+var _ Plugin = &LinePainter{}
+
+var LinePainterPlugin Plugin = &LinePainter{
+	data:    newLineData(),
+	enabled: false,
+}
+
+const (
+	LineMode        string = "line"
+	LineDescription string = "Create a line waveform"
+)
+
+type LinePainter struct {
+	data    *lineData
+	enabled bool
+}
+
+func (l *LinePainter) Name() string {
+	return LineMode
+}
+
+func (l *LinePainter) Description() string {
+	return LineDescription
+}
+
+func (l *LinePainter) Enabled() *bool {
+	return &l.enabled
+}
+
+func (l *LinePainter) Data() interface{} {
+	return l.data
+}
+
+func (l *LinePainter) Validate() error {
+	errs := l.data.ValidateLineOptions()
+	errlist := cmdutils.NewErrorList(errs)
+	if errlist == nil {
+		return nil
+	}
+	return errors.New(errlist.Error())
+}
+
+func (l *LinePainter) Flags(flags *pflag.FlagSet) {
+	data, ok := l.Data().(lineData)
+	if !ok {
+		log.Fatal().Msg("line data struct is malformed")
+	}
+	flags.BoolVar(l.Enabled(), LineMode, false, LineDescription)
+	flags.StringVar(&data.interpolation, options.Interpolation, string(line.DefaultInterpolation), options.InterpolationDescription)
+	flags.StringVar(&data.fill, options.LineFill, line.DefaultFillColor, options.LineFillDescription)
+	flags.StringVar(&data.strokeColor, options.StrokeColor, line.DefaultStrokeColor, options.StrokeColorDescription)
+	flags.StringVar(&data.strokeWidth, options.StrokeWidth, line.DefaultStrokeWidth, options.StrokeWidthDescription)
+	flags.Float64VarP(&data.spread, options.LineSpread, options.LineSpreadShort, line.DefaultSpread, options.LineSpreadDescription)
+	flags.Float64VarP(&data.height, options.LineHeight, options.LineHeightShort, line.DefaultHeight, options.LineHeightDescription)
+	flags.BoolVarP(&data.closed, options.Closed, options.ClosedShort, false, options.ClosedDescription)
+	flags.BoolVarP(&data.inverted, options.Inverted, options.InvertedShort, false, options.InvertedDescription)
+}
+
+func (l *lineData) ValidateLineOptions() (errList []error) {
+	if err := validation.ValidateInterpolation(l.interpolation); err != nil {
+		errList = append(errList, err)
+	}
+	if err := validation.ValidateSpread(l.spread); err != nil {
+		errList = append(errList, err)
+	}
+	if err := validation.ValidateLineHeight(l.height); err != nil {
+		errList = append(errList, err)
+	}
+	if len(errList) == 0 {
+		return nil
+	}
+	return errList
+}
 
 type lineData struct {
 	interpolation string
@@ -25,7 +107,3 @@ func newLineData() *lineData {
 		inverted:      false,
 	}
 }
-
-const (
-	LineMode string = "line"
-)
